2b4fcfe0c3dc5f42da0d035119b0e14a431b97f9
aum9518 <abduganiuulumanasbek@gmail.com>
2023-02-08 17:31:05 +0600

    add Reader

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Reader reader = new Reader("Abdugani uulu Manasbek",1243233,"Filasofia", LocalDate.of(1995,8,26),221788717);
+        System.out.println("Чтобы взять книги нажмите 1\nЧтобы вернуть книги нажмите 2");
+    int a = new Scanner(System.in).nextInt();
+        if (a==1){
+            System.out.println("Введите количество книг который вы хотели взять");
+            reader.takeBook(new Scanner(System.in).nextInt());
+        } else if (a==2) {
+            System.out.println("Введите количество книг который вы хотели вернуть");
+            reader.returnBook(new Scanner(System.in).nextInt());
+        }
+
     }
 }
--- src/Reader.java
+++ src/Reader.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Reader {
+import java.time.LocalDate;
+
+public class Reader {
+    String fullName;
+    int lybraryCardNumber;
+    String faculty;
+    LocalDate dateOfBirth;
+    int phoneNumber;
+
+    public Reader(String fullName, int lybraryCardNumber, String faculty, LocalDate dateOfBirth, int phoneNumber) {
+        this.fullName = fullName;
+        this.lybraryCardNumber = lybraryCardNumber;
+        this.faculty = faculty;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void takeBook(int number){
+        System.out.println(fullName+" взял "+ number+" книги");
+
+    }
+    public void returnBook(int num){
+        System.out.println(fullName+" вернул "+ num+" книги");
+    }
 }
